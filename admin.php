e2d7fcb0cc8f6fc3bfe098929b144a99dafcd410
fxshxr <igor.lytkin@gmail.com>
2020-06-25 09:50:12 +0300

    1st form

---
+++ admin.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Админ-панель - аналитика вопросов - экзамен php</title>
+</head>
+<body>
+	
+</body>
+</html>
--- index.php
+++ index.php
@@ -5,6 +5,20 @@
 	<title>Экзамен PHP - Аналитика запросов</title>
 </head>
 <body>
-	
+	<h1>Ответьте на вопросы ниже</h1>
+
+	<form action="">
+		<label for="">Введите число <input type="text"></label>
+		<br>
+		<label for=""> Введите положительное число больше 0<input type="text"></label>
+		<br>
+		<label for=""> Введите ответ (не более 30 символов)<input type="text"></label>
+		<br>
+		<label for=""> Введите ответ (не более 255 символов)<input type="text"></label>
+		<br>
+		<label for="">Выберете вариант ответа<input type="text"></label>
+		<br>
+		<label for="">Выберете несколько вариантов ответа<input type="text"></label>
+	</form>
 </body>
 </html>
